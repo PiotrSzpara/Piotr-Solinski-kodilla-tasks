0d1173973aa049c4133403481d87081e977c43f1
Piotr Szpara-Soliński <72048074+PiotrSzpara@users.noreply.github.com>
2021-01-12 22:13:09 +0100

    18.4 TaskController correction

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -15,7 +15,7 @@ public class TaskController {
     public List<TaskDto> getTasks() {
         return new ArrayList<>();
     }
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long taskId) {
         return new TaskDto(1L, "test title", "test_content");
     }
