9956b6313917e4bece225b83185fab29b965c5a2
Piotr Szpara-Soliński <72048074+PiotrSzpara@users.noreply.github.com>
2021-02-10 19:00:43 +0100

    TrelloController correction 22.2

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -20,9 +20,18 @@ public class TrelloController {
 
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-        trelloBoards
-                .stream().filter(e -> e.getName().contains("Kodilla"))
-                .forEach(trelloBoardDto -> {System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-        });
+        trelloBoards.stream()
+                .filter(e -> e.getId() != null)
+                .filter(e -> e.getName() != null)
+                .filter(e -> e.getName().contains("Kodilla"))
+
+                .forEach(trelloBoardDto -> {
+                    System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+                    System.out.println("This board contains lists: ");
+
+                    trelloBoardDto.getLists().forEach(trelloList -> { System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()); });
+
+                });
+
     }
 }
