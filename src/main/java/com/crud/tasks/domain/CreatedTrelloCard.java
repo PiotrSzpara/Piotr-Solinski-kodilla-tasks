d841f3433558d98a5c3bdb591df4f85bb3c21b51
Piotr Szpara-Soliński <72048074+PiotrSzpara@users.noreply.github.com>
2021-02-16 17:32:25 +0100

    Second attempt to Heroku

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -27,8 +27,8 @@ public class CreatedTrelloCard {
     @JsonProperty(" shortUrl ")
     private String shortUrl;
 
-    @JsonProperty(" badges ")
-    private Badges badges;
+    //@JsonProperty(" badges ")
+    //private Badges badges;
 
 
 
