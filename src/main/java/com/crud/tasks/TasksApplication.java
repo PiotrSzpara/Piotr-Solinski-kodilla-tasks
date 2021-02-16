0a86590d8c1ebd193ee8f41fc4a32002490a0d2c
Piotr Szpara-Soliński <72048074+PiotrSzpara@users.noreply.github.com>
2021-02-16 17:49:06 +0100

    Third attempt

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,8 +2,7 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 //public class TasksApplication extends SpringBootServletInitializer {
@@ -12,9 +11,8 @@ public class TasksApplication {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
-	//@Override
-	//protected SpringApplicationBuilder configure (SpringApplicationBuilder application)
-	//{
-		//return application.sources(TasksApplication.class);
-	//}
+/*	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
+		return application.sources(TasksApplication.class);
+	}*/
 }
